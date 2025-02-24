7504a5315615e999f8e7ee9965306423f59d5d84
Ahmad <Ahmadsalouka@gmail.com>
2025-02-24 19:33:57 +0100

    add json_format file and implement the code
    - a json_format file was created
    - a json_format function was implemented

---
+++ metaquill/src/json_format.rs
@@ -0,0 +1,9 @@
+use std::collections::HashMap;
+use serde_json:: {json,Value};
+
+fn json_format(key: &str, value: Value) -> Value {
+    let mut JSON = HashMap::new();
+    JSON.insert(key.to_string(),value);
+    serde_json::to_value(JSON).unwrap()
+}
+
