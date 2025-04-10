d2c6bcd41b7c75eaa9f95534dae3eb89a769c6ad
Casper Aborres <casper.aborres@gmail.com>
2025-04-10 21:47:27 +0200

    Added so that batch system adds all pdf metadata to the json file

--- metaquill/src/file_manager.rs
+++ metaquill/src/file_manager.rs
@@ -1,9 +1,10 @@
 use crate::call::Metadata;
 use std::fs::File;
-use std::io::{BufWriter, Write};
+use std::io::{Seek, SeekFrom, Write, BufWriter};
 use serde_json::{json, Value};
 use lopdf::{Document, Error};
 use crate::metadata::PDFStruct;
+use std::fs::OpenOptions;
 
 pub fn load_pdf(filepath : &str) -> Result<Document, Error> {
     // println!("Loading PDF {filepath}");
@@ -34,7 +35,7 @@ pub fn export_json(extracted_meta: &Metadata, filepath: &str) {
     println!("{}", serde_json::to_string_pretty(&json_value).unwrap());
 
     // Save JSON to a file
-    if let Err(e) = create_file(json_value) {
+    if let Err(e) = create_file_append(json_value) {
         eprintln!("Error creating file: {}", e);
     }
 }
@@ -73,17 +74,49 @@ pub fn export_json_metadata(pdf_metadata : &PDFStruct){
     println!("{}", serde_json::to_string_pretty(&json_value).unwrap());
 
     // Save JSON to a file
-    if let Err(e) = create_file(json_value) {
+    if let Err(e) = create_file_append(json_value) {
         eprintln!("Error creating file: {}", e);
     }
 }
 
-pub fn create_file(value: Value) -> std::io::Result<()> {
+pub fn create_file() -> std::io::Result<()> {
     let file = File::create("output.json")?;
     let mut writer = BufWriter::new(file);
+    writer.write_all(b"[\n")?;
+    writer.flush()?;
+    Ok(())
+}
 
-    let json_string = serde_json::to_string_pretty(&value)?; // Serialize JSON to string
+pub fn close_file() -> std::io::Result<()> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open("output.json")?;
+
+    // Move back 2 bytes to remove the last ','
+    let file_len = file.metadata()?.len();
+    if file_len >= 2 {
+        file.seek(SeekFrom::End(-2))?;
+        file.set_len(file_len - 2)?; // Truncate last comma and newline
+    }
+
+    // Now append the closing bracket
+    let mut writer = BufWriter::new(file);
+    writer.write_all(b"\n]")?;
+    writer.flush()?;
+    Ok(())
+}
+
+pub fn create_file_append(value: Value) -> std::io::Result<()> {
+    let file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open("output.json")?;
+    let mut writer = BufWriter::new(file);
+
+    let json_string = serde_json::to_string_pretty(&value)?;
     writer.write_all(json_string.as_bytes())?;
+    writer.write_all(b",\n")?;
     writer.flush()?;
 
     Ok(())
